7c3a3bbd23bacc395adb744bc841bc49cd3db251
RolandD13 <rolandoroz@gmail.com>
2022-03-15 13:01:00 +0100

    changing project structure

--- src/app/components/article-list/article-list-item/article-list-item.component.ts
+++ src/app/components/article-list/article-list-item/article-list-item.component.ts
@@ -9,35 +9,11 @@ import { ArticleInterface } from "../../../model/article/articleInterface";
 export class ArticleListItemComponent implements OnInit {
 
 
-  articleList: ({
-    articleTitle: string;
-    articleUuid: number;
-    articleMedia: string;
-    articleDate: string;
- })[] = [
-   {
-    articleTitle: 'Title1',
-    articleUuid: 1,
-    articleMedia: 'MediaType1',
-    articleDate: '2010-01-05T11:08:05.035Z'
-   },
-    {
-      articleTitle: 'Title2',
-      articleUuid: 2,
-      articleMedia: 'MediaType2',
-      articleDate: '2017-08-15T13:16:05.035Z'
-    },
-    {
-      articleTitle: 'Title3',
-      articleUuid: 3,
-      articleMedia: 'MediaType3',
-      articleDate: '2020-03-21T19:02:05.035Z'
-    },
-  ];
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
