5735404c5465d75bf111fa75fa0b6ee18f011873
RolandD13 <rolandoroz@gmail.com>
2022-04-04 11:45:56 +0200

    setting hours to 0, using Date FNS js

--- src/app/components/filters/datetime-picker/datetime-picker.component.ts
+++ src/app/components/filters/datetime-picker/datetime-picker.component.ts
@@ -1,8 +1,9 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {MatDatepickerInputEvent} from "@angular/material/datepicker";
 import {DateSelection} from "../../../model/dateSelection";
-
 import {IDateSelection} from "../../../model/Interface/IDateSelection";
+import {addDays, startOfDay, subDays, subMonths, subWeeks} from 'date-fns';
+
 
 //date fns js date library if used____as alternative to moment js
 
@@ -16,9 +17,11 @@ import {IDateSelection} from "../../../model/Interface/IDateSelection";
 export class DatetimePickerComponent implements OnInit {
 
   collapsed!: boolean;
-  selectedDay = new Date();
-  startDate!: Date;
-  endDate!: Date | null;
+
+
+  selectedDay = new Date(new Date().setHours(0,0,0,0));
+  startDate = new Date(new Date().setHours(0,0,0,0));
+  endDate = new Date(new Date().setHours(0,0,0,0));
 
   constructor() {
   }
@@ -26,8 +29,8 @@ export class DatetimePickerComponent implements OnInit {
   @Output() onRangeSelected = new EventEmitter<IDateSelection>();
 
   dateFilter_prevDay() {
-    let prevDay = new Date(this.selectedDay);
-    let prevDayEnd = new Date(this.selectedDay);
+    let prevDay = startOfDay(this.selectedDay);
+    let prevDayEnd = startOfDay(prevDay);
     this.selectedDay = prevDay;
     prevDay.setDate(this.selectedDay.getDate()-1);
     this.onRangeSelected.emit(DateSelection.of(
@@ -40,17 +43,17 @@ export class DatetimePickerComponent implements OnInit {
     if (this.selectedDay >= maxDate) {
       return;
     }
-    let nextDay = new Date(this.selectedDay);
-    let nextDayEnd = this.selectedDay.setDate(nextDay.getDate()+2);
+    let nextDay = startOfDay(this.selectedDay);
+    let nextDayEnd = addDays((nextDay),2);
     this.selectedDay = nextDay;
     nextDay.setDate(this.selectedDay.getDate()+1);
     this.onRangeSelected.emit(DateSelection.of(
       nextDay.setDate(this.selectedDay.getDate()),
-      nextDayEnd));
+      nextDayEnd.getTime()));
   }
 
   addDateEvent(type: string, input: MatDatepickerInputEvent<unknown, unknown | null>) {
-    let singleDate = new Date(this.selectedDay);
+    let singleDate = startOfDay(this.selectedDay);
     let tempSingleDate = new Date(this.selectedDay);
     let singleDateEnd = singleDate.setDate(tempSingleDate.getDate()+1);
     singleDate.setDate(this.selectedDay.getDate())
@@ -65,14 +68,14 @@ export class DatetimePickerComponent implements OnInit {
 
   startRangeChange(event: MatDatepickerInputEvent<any>) {
     this.startDate = event.value;
-    this.endDate = null;
+    this.endDate = event.value;
     this.onRangeSelected.emit(DateSelection.of(
       this.startDate.getTime(),
       this.endDate.getTime()));
   }
 
   endRangeChange(event: MatDatepickerInputEvent<any>) {
-    this.endDate = event.value;
+    this.endDate = addDays((event.value),1);
     if (this.startDate != null && this.endDate != null) {
       this.onRangeSelected.emit(DateSelection.of(
         this.startDate.getTime(),
@@ -81,41 +84,40 @@ export class DatetimePickerComponent implements OnInit {
   }
 
   today() {
-    let today = new Date(new Date().setDate(new Date().getDate()));
-    let todayEnd = new Date(new Date().setDate(new Date().getDate() + 1));
+    let today = startOfDay(new Date());
+    let todayEnd = addDays((today), 1);
     this.selectedDay = today;
     this.onRangeSelected.emit(DateSelection.of(today.getTime(), todayEnd.getTime()));
   }
 
   yesterday() {
-    let yesterday = new Date(new Date().setDate(new Date().getDate() - 1));
-    let yesterdayEnd = new Date(new Date().setDate(new Date().getDate()))
-    this.selectedDay = yesterday;
-    this.onRangeSelected.emit(DateSelection.of(yesterday.getTime(), yesterdayEnd.getTime()));
+    let yesterday = subDays(new Date(), 1).setHours(0,0,0,0);
+    let tempYesterday = subDays(new Date(), 1);
+    let yesterdayEnd = new Date(new Date().setHours(0,0,0,0));
+    this.selectedDay = tempYesterday;
+    this.onRangeSelected.emit(DateSelection.of(yesterday, yesterdayEnd.getTime()));
   }
 
   lastWeek() {
+    let lastWeek = startOfDay(new Date());
+    let lastWeekEnd = addDays((new Date()),1);
+    lastWeekEnd = startOfDay(lastWeekEnd);
+    this.selectedDay = subWeeks(new Date(),1);
+    lastWeek.setDate(this.selectedDay.getDate());
 
-     // this.selectedDay = new Date(new Date().setDate(new Date().getDate() - 7));
-    let lastWeek = new Date(this.selectedDay);
-    let lastWeekEnd = new Date().getTime();
-    this.selectedDay = new Date(new Date().setDate(new Date().getDate() - 7));
-    lastWeek.setDate(this.selectedDay.getDate())
+
     this.onRangeSelected.emit(DateSelection.of(
       lastWeek.getTime(),
-      lastWeekEnd));
+      lastWeekEnd.getTime()));
   }
 
   lastMonth() {
-    this.selectedDay = new Date(new Date().setMonth(new Date().getMonth() - 1));
-
-    let lastMonth = new Date(this.selectedDay);
-    let lastMonthEnd = new Date().getTime();
-    this.selectedDay = new Date(new Date().setMonth(new Date().getMonth() - 1));
-    lastMonth.setDate(this.selectedDay.getDate())
+    let lastMonth = subMonths(new Date(),1 ).setHours(0,0,0,0);
+    let lastMonthEnd = addDays((new Date()),1);
+    lastMonthEnd = startOfDay(lastMonthEnd);
     this.onRangeSelected.emit(DateSelection.of(
-      lastMonth.getTime(),
-      lastMonthEnd));
+      lastMonth,
+      lastMonthEnd.getTime()));
   }
 
   ngOnInit(): void {
