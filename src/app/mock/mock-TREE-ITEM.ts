fe9660995fe65ced524fb78b5f31e66bc0ed7e4f
RolandOroz <rolandoroz@gmail.com>
2022-04-28 08:12:11 +0200

    search bar

--- src/app/mock/mock-TREE-ITEM.ts
+++ src/app/mock/mock-TREE-ITEM.ts
@@ -3,7 +3,4 @@ import {FilterTreeItem} from "../model/FilterTreeItem";
 export const TREEITEM: FilterTreeItem = {
   uuid: 'uuid0101',
   name: 'Topics',
-  // andFT: false,
-  // orFT: false,
-  // notFT: true
 }
--- src/app/mock/mock-TREE-ITEMS.ts
+++ src/app/mock/mock-TREE-ITEMS.ts
@@ -4,36 +4,21 @@ export const TREEITEMS: FilterTreeItem[] = [
   {
     uuid: 'uuid02',
     name: 'item2',
-    // andFT: true,
-    // orFT: false,
-    // notFT: true
   },
   {
     uuid: 'uuid03',
     name: 'item3',
-    // andFT: false,
-    // orFT: false,
-    // notFT: true
   },
   {
     uuid: 'uuid04',
     name: 'item4',
-    // andFT: true,
-    // orFT: true,
-    // notFT: false
   },
   {
     uuid: 'uuid05',
     name: 'item5',
-    // andFT: true,
-    // orFT: false,
-    // notFT: true
   },
   {
     uuid: 'uuid06',
     name: 'item6',
-    // andFT: true,
-    // orFT: false,
-    // notFT: true
   }
 ]
